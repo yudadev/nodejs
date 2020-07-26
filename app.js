f5a2a26064ba0da23dcf8bdb1d6a9e876af09fc8
yudadev <daniel.yu.webdev@gmail.com>
2020-07-26 14:32:36 -0400

    update

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require("http");
 const port = process.env.PORT || 3000;
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello. This page is running Node.js version: ");
-	response.write(process.version);
-	response.end();
-}).listen(port);
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+	console.log(`Server running at port `+port);
+});
