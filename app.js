1955b4fa551da18b5201a363835b0aa251a03607
yudadev <daniel.yu.webdev@gmail.com>
2020-07-26 14:29:38 -0400

    update port

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var http = require("http");
+const http = require("http");
+const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello. This page is running Node.js version: ");
 	response.write(process.version);
 	response.end();
-}).listen(8888);
+}).listen(port);
